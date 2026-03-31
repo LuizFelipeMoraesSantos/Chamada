7548bcd4ec2afc9ea1a1ba34ab2f97e5e4238f4d
LuizFelipeMoraesSantos <127338869+LuizFelipeMoraesSantos@users.noreply.github.com>
2026-03-31 00:13:08 -0300

    criação do banco de dados H2 para testes.

--- SENAC/SENAC/src/main/java/com/SENAC/SENAC/model/AlunoModel.java
+++ SENAC/SENAC/src/main/java/com/SENAC/SENAC/model/AlunoModel.java
@@ -3,6 +3,9 @@ package com.SENAC.SENAC.model;
 import java.sql.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,9 +20,11 @@ import lombok.Setter;
 @Table(name="Alunos")
 @Entity
 public class AlunoModel {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int codigo;
     private String nome;
+    private int presenca;
     private Date diasDaSemana;
     private Date mes;
     private int diasLetivos;
